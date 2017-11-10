80566f683d509cde0235836183f9b2095962a1c8
Adriano Gimenez <agimenez95@gmail.com>
2017-11-10 14:04:02 +0000

    registration and login adds

---
+++ view/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Blogger</title>
+  </head>
+  <body>
+    <header>
+      <h1>Blogger</h1>
+    </header>
+    <div class="blogs">
+      <p>blog</p>
+    </div>
+  </body>
+</html>
